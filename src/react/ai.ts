642d62fe655ac29a3af59f30fc09923fef273724
kamilmac <kmacinski@gmail.com>
2024-02-28 17:57:06 +0100

    +

---
+++ src/react/ai.ts
@@ -0,0 +1,43 @@
+import { CFG } from "../config";
+import { appState } from "../state";
+
+const RANDOM_ACTIONS: (keyof typeof CFG.controls)[] = [
+	"left",
+	"right",
+	"up",
+	"down",
+	"left",
+	"right",
+	"up",
+	"down",
+	"left",
+	"right",
+	"up",
+	"down",
+	"fall",
+	"fall",
+	"rotate",
+	"rotate",
+	"rotate",
+	"rotate",
+	"camera_rotate_right",
+	"camera_rotate_left",
+];
+
+export const initAI = () => {
+	setInterval(() => {
+		if (appState.state.status !== "inDemo") {
+			return;
+		}
+		const chance = 0.1;
+		if (Math.random() < chance) {
+			const randomAction =
+				RANDOM_ACTIONS[Math.floor(Math.random() * RANDOM_ACTIONS.length)];
+			const event = CFG.controls[randomAction];
+			if (!event) {
+				return;
+			}
+			document.dispatchEvent(event);
+		}
+	}, 40);
+};
