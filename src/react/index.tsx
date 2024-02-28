2c8d44c2a4fd40378a2d04caa7885477318339fe
kamilmac <kmacinski@gmail.com>
2024-02-28 17:53:20 +0100

    not lit

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -8,6 +8,44 @@ import { Score } from "./score";
 import { appState } from "../state";
 
 import './styles.css';
+import { CFG } from "../config";
+
+const RANDOM_ACTIONS: (keyof typeof CFG.controls)[] = [
+	"left",
+	"right",
+	"up",
+	"down",
+	"left",
+	"right",
+	"up",
+	"down",
+	"fall",
+	"fall",
+	"rotate",
+	"rotate",
+	"rotate",
+	"rotate",
+	"camera_rotate_right",
+	"camera_rotate_left",
+];
+
+const initAI = () => {
+	setInterval(() => {
+		if (appState.state.status !== "inDemo") {
+			return;
+		}
+		const chance = 0.1;
+		if (Math.random() < chance) {
+			const randomAction =
+				RANDOM_ACTIONS[Math.floor(Math.random() * RANDOM_ACTIONS.length)];
+			const event = CFG.controls[randomAction];
+			if (!event) {
+				return;
+			}
+			document.dispatchEvent(event);
+		}
+	}, 40);
+};
 
 const App = () => {
 	const [showLegend, setShowLegend] = React.useState(true);
@@ -20,6 +58,10 @@ const App = () => {
 			}, 200);
     }
   };
+
+  React.useEffect(() => {
+  	initAI();
+  }, []);
 
 	return (
 		<>
